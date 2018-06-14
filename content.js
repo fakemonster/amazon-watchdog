8a2de526e098e54242069f0ab0e93eb39227fd0a
Joe Thel <jgraythel@gmail.com>
2018-06-14 01:42:29 -0700

    ✨ (amazon-watchdog): fuck yeah

--- content.js
+++ content.js
@@ -1,26 +1,9 @@
-let url = window.location.href;
-// url = url.slice(0, url.indexOf('?'));
-// url = encodeURIComponent(url)
-// const target = `https://www.fakespot.com/analyze?utf8=%E2%9C%93&url=${url}`;
-// const target = 'https://www.fakespot.com/product/dewalt-dwfp55126-6-gallon-165-psi-pancake-compressor';
-// console.log(target);
+const url = window.location.href;
 
 const base = 'https://camelcamelcamel.com//product/'
 const path = url.match(/dp\/\w*/)[0].slice(3);
 const chart = `https://charts.camelcamelcamel.com/us/${path}/amazon.png?force=1&zero=0&w=400&h=243&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=en`;
 const itemTitle = document.getElementById('productTitle').innerHTML.trim();
-const itemSlug = itemTitle.replace(/[^a-zA-Z0-9 ]/g, '').replace(/ /g, '-').toLowerCase();
+const itemSlug = itemTitle.replace(/[^a-zA-Z0-9 \-.]/g, '').replace(/[ .]/g, '-').toLowerCase();
 
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === 4) {
-    console.log('hey!');
-    console.log(xhr.responseText);
-  }
-}
-xhr.open('GET', base+path, true);
-xhr.send();
-
-
-chrome.runtime.sendMessage({location: chart, itemSlug}, (response) => {
-});
+chrome.runtime.sendMessage({location: chart, itemSlug}, (response) => { });
--- popup.js
+++ popup.js
@@ -1,56 +1,42 @@
 
-
-document.getElementById('test').innerHTML = window.location.href;
-
-// const changeColor = document.getElementById('changeColor');
-
-// chrome.storage.sync.get('color', (data) => {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-
-// changeColor.onclick = (event) => {
-//   const color = event.target.value;
-//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     chrome.tabs.executeScript(
-//       tabs[0].id,
-//       { code: `document.body.style.backgroundColor = "${color}";` },
-//     );
-//   });
-// };
-
-// chrome.runtime.onMessage.addListener (
-//   function(request, sender, sendResponse) {
-//     console.log('got a message')
-//   }
-// )
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(request);
-    fetch(`http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=${APIKEY}`)
+    fetch(`https://www.fakespot.com/product/${request.itemSlug}`)
       .then((response) => {
-        console.log(response);
-        response.json().then(body => {
-          console.log(body);
-          let image = body.data[0].images.fixed_height_small.url;
-          let giphy = document.createElement('img');
-          giphy.src = image;
-          document.getElementById('c3').appendChild(giphy);
-        }).catch(console.log);
-      })
-      .catch(console.log);
+        response.text()
+          .then(text => {
+            let temp = text;
+            const words = [];
+            while (temp.match(/<i>\w*<\/i>/)) {
+              let match = temp.match(/<i>\w*<\/i>/);
+              words.push(match[0]);
+              temp = temp.slice(match.index + 7);
+            }
+            const sanitizedWords = words.join('+').replace(/<\/?i>/g, '');
+            const displayWords = sanitizedWords.replace(/\+/g, ', ');
+            const adjectives = document.createElement('p');
+            adjectives.innerHTML = `Users mentioned that this product is: ${displayWords}`;
+            document.getElementById('c3').appendChild(adjectives);
+
+            fetch(`http://api.giphy.com/v1/gifs/search?q=${sanitizedWords}&api_key=${APIKEY}`)
+              .then((response) => {
+                response.json().then(body => {
+                  let image = body.data[0].images.original.url;
+                  let giphy = document.createElement('img');
+                  giphy.src = image;
+                  giphy.className = 'gif';
+                  document.getElementById('c3').appendChild(giphy);
+                }).catch(console.log);
+              }).catch(console.log);
+          }).catch(console.log);
+      }).catch(console.log);
+
     let chart = document.createElement('img');
     chart.src = request.location;
     document.getElementById('c3').appendChild(chart);
 
-    sendResponse({farewell: 'goodbye'});
   }
 )
 
 
 
-// chrome.tabs.executeScript(
-//   tabs[0].id,
-//   {code: `document.getElementById('test').innerHTML(window.location.href)`}
-// )
