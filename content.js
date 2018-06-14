92d038c10a7bf462f118897f1469a282e4cdb823
Joe Thel <jgraythel@gmail.com>
2018-06-14 14:22:19 -0700

    ♻  (popup): send first message from popup instead of content script

--- content.js
+++ content.js
@@ -4,6 +4,9 @@ const base = 'https://camelcamelcamel.com//product/'
 const path = url.match(/dp\/\w*/)[0].slice(3);
 const chart = `https://charts.camelcamelcamel.com/us/${path}/amazon.png?force=1&zero=0&w=400&h=243&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=en`;
 const itemTitle = document.getElementById('productTitle').innerHTML.trim();
-const itemSlug = itemTitle.replace(/[^a-zA-Z0-9 \-.]/g, '').replace(/[ .]/g, '-').replace(/-+/g,'-').toLowerCase();
+const itemSlug = itemTitle.replace(/[^a-zA-Z0-9 \-.]/g, '').replace(/[ .]/g, '-').replace(/-+/g, '-').toLowerCase();
 
-chrome.runtime.sendMessage({location: chart, itemSlug}, (response) => { });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('sending message to extension');
+  sendResponse({ location: chart, itemSlug });
+});
--- popup.js
+++ popup.js
@@ -1,44 +1,47 @@
 const APIKEY = '';
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    fetch(`https://www.fakespot.com/product/${request.itemSlug}`)
-      .then((response) => {
-        response.text()
-          .then(text => {
-            let temp = text;
-            const words = [];
-            while (temp.match(/<i>\w*<\/i>/)) {
-              let match = temp.match(/<i>\w*<\/i>/);
-              words.push(match[0]);
-              temp = temp.slice(match.index + 7);
-            }
-            const sanitizedWords = words.join('+').replace(/<\/?i>/g, '');
-            const displayWords = sanitizedWords.replace(/\+/g, ', ');
-            const adjectives = document.createElement('p');
-            const productDescription = request.itemSlug.split('-').slice(0, 3).join('+');
-            adjectives.innerHTML = `Users mentioned that this product is: ${displayWords}`;
-            document.getElementById('c3').appendChild(adjectives);
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const activeTab = tabs[0];
+  chrome.tabs.sendMessage(
+    tabs[0].id,
+    { message: 'page detail' },
+    function queryTime(tabResponse) {
+      fetch(`https://www.fakespot.com/product/${tabResponse.itemSlug}`)
+        .then((fakeSpotResponse) => {
+          fakeSpotResponse.text()
+            .then((text) => {
+              let temp = text;
+              const words = [];
+              while (temp.match(/<i>\w*<\/i>/)) {
+                const match = temp.match(/<i>\w*<\/i>/);
+                words.push(match[0]);
+                temp = temp.slice(match.index + 7);
+              }
+              const sanitizedWords = words.join('+').replace(/<\/?i>/g, '');
+              const displayWords = sanitizedWords.replace(/\+/g, ', ');
+              const adjectives = document.createElement('p');
+              const productDescription = tabResponse.itemSlug.split('-').slice(0, 3).join('+');
+              adjectives.innerHTML = `Users mentioned that this product is: ${displayWords}`;
+              document.getElementById('c3').appendChild(adjectives);
 
-            fetch(`http://api.giphy.com/v1/gifs/search?q=${sanitizedWords ? sanitizedWords : productDescription}&api_key=${APIKEY}`)
-              .then((response) => {
-                response.json().then(body => {
-                  let image = body.data[0].images.original.url;
-                  let giphy = document.createElement('img');
-                  giphy.src = image;
-                  giphy.className = 'gif';
-                  document.getElementById('c3').appendChild(giphy);
+              fetch(`http://api.giphy.com/v1/gifs/search?q=${sanitizedWords || productDescription}&api_key=${APIKEY}`)
+                .then((giphyResponse) => {
+                  giphyResponse.json()
+                    .then(body => {
+                      const image = body.data[0].images.original.url;
+                      const giphy = document.createElement('img');
+                      giphy.src = image;
+                      giphy.className = 'gif';
+                      document.getElementById('c3').appendChild(giphy);
+                    }).catch(console.log);
                 }).catch(console.log);
-              }).catch(console.log);
-          }).catch(console.log);
-      }).catch(console.log);
+            }).catch(console.log);
+        }).catch(console.log);
 
-    let chart = document.createElement('img');
-    chart.src = request.location;
-    document.getElementById('c3').appendChild(chart);
-
-  }
-)
-
-
+      let chart = document.createElement('img');
+      chart.src = tabResponse.location;
+      document.getElementById('c3').appendChild(chart);
+    },
+  )
+});
 
