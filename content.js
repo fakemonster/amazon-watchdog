576291c7db0a0ff9cfe242dcebce8ca09ba14fe1
Joe Thel <jgraythel@gmail.com>
2018-06-14 11:33:35 -0700

    🐛

--- content.js
+++ content.js
@@ -4,6 +4,6 @@ const base = 'https://camelcamelcamel.com//product/'
 const path = url.match(/dp\/\w*/)[0].slice(3);
 const chart = `https://charts.camelcamelcamel.com/us/${path}/amazon.png?force=1&zero=0&w=400&h=243&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=en`;
 const itemTitle = document.getElementById('productTitle').innerHTML.trim();
-const itemSlug = itemTitle.replace(/[^a-zA-Z1-9 \-.]/g, '').replace(/[ .]/g, '-').replace(/-+/g,'-').toLowerCase();
+const itemSlug = itemTitle.replace(/[^a-zA-Z0-9 \-.]/g, '').replace(/[ .]/g, '-').replace(/-+/g,'-').toLowerCase();
 
 chrome.runtime.sendMessage({location: chart, itemSlug}, (response) => { });
